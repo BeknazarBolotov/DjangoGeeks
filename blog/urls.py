7a3b45cd337faa04d3fe4e92b9d829558366a043
BeknazarBolotov <bolotov.beknazar08@gmail.com>
2025-03-21 15:05:50 +0600

    HW1

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import test_view
+from posts.views import test_view, html_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/',test_view)
+    path('test/', test_view),
+    path('html/', html_view)
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def test_view(request):
     return HttpResponse("Hello World!")
+
+
+def html_view(request):
+    return render(request, "posts/test.html")
+    
