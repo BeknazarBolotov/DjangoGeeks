5fdf5872344cf008ff6d1e03474f32a928125c5e
BeknazarBolotov <bolotov.beknazar08@gmail.com>
2025-03-21 16:16:20 +0600

    HW2

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import Post
 
 
 def test_view(request):
     return HttpResponse("Hello World!")
 
 
-def html_view(request):
-    return render(request, "posts/test.html")
+def home_page_view(request):
+    return render(request, "base.html")
     
+
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "posts/post_list.html", context = {"posts":posts})
