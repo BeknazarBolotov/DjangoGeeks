4d3034c028347af6a007cbdee73a16c4a71902a2
BeknazarBolotov <160572129+BeknazarBolotov@users.noreply.github.com>
2025-04-03 07:17:28 +0000

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', TestView.as_view()),
     path('', home_page_view),
-    path('posts/class/', PostListView2.as_view()),
+    path('posts/', post_list_view),
     path('posts/<int:post_id>/', post_detail_view),
     path('posts/create/', post_create_view),
     path('register/', register_view),
